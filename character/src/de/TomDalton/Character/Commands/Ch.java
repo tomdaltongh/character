4f85cc1ca034748cc185b9ffc8d4ffd18ca0a0f3
tomdalton <email@tomdalton.de>
2021-06-27 16:43:36 +0200

    spätere änderung verhindert

--- character/src/de/TomDalton/Character/Commands/Ch.java
+++ character/src/de/TomDalton/Character/Commands/Ch.java
@@ -1,6 +1,7 @@
 package de.TomDalton.Character.Commands;
 
 import java.io.File;
+import java.util.Random;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -38,39 +39,76 @@ public class Ch implements CommandExecutor{
 		 *  /ch set vorname *vorname* (String)
 		 *  /ch set alter *alter* (int)
 		 *  /ch set beruf *beruf* (String/jederzeit ausführbar)
+		 *  /ch erbe *Ausweisnummer*
 		 */
 		if(sender instanceof Player) {
 			Player p = (Player) sender;
 			if(args.length==3) {
 				if(args[0].equalsIgnoreCase("set")) {
 					String mode = args[1];
-					if(mode.equalsIgnoreCase("rpname")) {
-						userdata.set("userdata."+p.getDisplayName()+".name", args[2]);
-						sender.sendMessage(prefix+" Der Name "+args[2]+" wurde gespeichert.");
-					}else if(mode.equalsIgnoreCase("vorname")) {
-						userdata.set("userdata."+p.getDisplayName()+".vorname", args[2]);
-						sender.sendMessage(prefix+" Der Vorname "+args[2]+" wurde gespeichert.");
-					}else if(mode.equalsIgnoreCase("alter")) {
-						userdata.set("userdata."+p.getDisplayName()+".alter", args[2]);
-						sender.sendMessage(prefix+" Das Alter "+args[2]+" wurde gespeichert.");
-					}else if(mode.equalsIgnoreCase("beruf")) {
-						userdata.set("userdata."+p.getDisplayName()+".beruf", args[2]);
-						sender.sendMessage(prefix+" Der Beruf "+args[2]+" wurde gespeichert.");
+					if(!signed(p)) {
+						if(mode.equalsIgnoreCase("rpname")) {
+							userdata.set("userdata."+p.getDisplayName()+".name", args[2]);
+							sender.sendMessage(prefix+" Der Name "+args[2]+" wurde gespeichert.");
+						}else if(mode.equalsIgnoreCase("vorname")) {
+							userdata.set("userdata."+p.getDisplayName()+".vorname", args[2]);
+							sender.sendMessage(prefix+" Der Vorname "+args[2]+" wurde gespeichert.");
+						}else if(mode.equalsIgnoreCase("alter")) {
+							userdata.set("userdata."+p.getDisplayName()+".alter", args[2]);
+							sender.sendMessage(prefix+" Das Alter "+args[2]+" wurde gespeichert.");
+						}else if(mode.equalsIgnoreCase("beruf")) {
+							userdata.set("userdata."+p.getDisplayName()+".beruf", args[2]);
+							sender.sendMessage(prefix+" Der Beruf "+args[2]+" wurde gespeichert.");
+						}else {
+							sender.sendMessage(prefix+" Diese Kategorie wurde noch nicht eingerichtet.");
+						}
 					}else {
-						sender.sendMessage(prefix+" Diese Kategorie wurde noch nicht eingerichtet.");
+						if(mode.equalsIgnoreCase("beruf")) {
+							userdata.set("userdata."+p.getDisplayName()+".beruf", args[2]);
+							sender.sendMessage(prefix+" Der Beruf "+args[2]+" wurde gespeichert.");
+						}else {
+							sender.sendMessage(prefix+" Diese Kategorie wurde noch nicht eingerichtet.");
+						}
 					}
 					Api.saveData(userdata, data);
-					sender.sendMessage(prefix+" Überprüfe deine Angaben auf Richtigkeit und korrigiere, falls nötig.");
+					sender.sendMessage(prefix+" (Info) Überprüfe deine Angaben auf Richtigkeit und korrigiere, falls nötig.");
 				}else if(args[0].equalsIgnoreCase("erstellen")) {
-					
+					p.sendMessage(prefix+" Führe die folgenden Befehle aus, um deinen RP-Charakter zu erstellen.");
+					p.sendMessage(prefix+" Bitte beachte bei der Erstellung die aktuellen RP-Richtlinien.");
+					p.sendMessage(prefix+" /ch set rpname *Name*");
+					p.sendMessage(prefix+" /ch set vorname *Vorname*");
+					p.sendMessage(prefix+" /ch set alter *Alter*");
+					p.sendMessage(prefix+" /ch set beruf *aktueller Beruf*");
+					p.sendMessage(prefix+" Mit dem Befehl /ch sign kannst du deine Angaben bestätigen und bist bestätigter Bürger.");
 				}else {
-					sender.sendMessage(prefix+" Benutze /ch erstellen um dir Hilfe anzeigen zu lassen.");
+					p.sendMessage(prefix+" Benutze /ch erstellen um dir Hilfe anzeigen zu lassen.");
+				}
+			}else if(args.length==1) {
+				if(args[0].equalsIgnoreCase("sign")) {
+					userdata.set("userdata."+p.getDisplayName()+".signed", "true");
+					
+					Api.saveData(userdata, data);
+					p.sendMessage(prefix+" Du hast deine Angaben bestätigt und kannst diese nun nicht mehr ändern.");
+					p.sendMessage(prefix+" >/ch set beruf *aktueller Beruf*< ist davon ausgeschlossen.");
+					p.sendMessage(prefix+" Du möchtest einen anderen Charakter erstellen? Nutze >/ch kill< um deinen Fortschritt zu löschen.");
+				}
+			}else if(args.length==2) {
+				if() {
+					
 				}
 			}
 		}else {
 			System.out.println(prefix + " Dieser Befehl funktioniert nur als Spieler.");
 		}
 		return false;
+	}
+	
+	public boolean signed(Player p) {
+		if(userdata.getString("userdata."+p.getDisplayName()+".signed").equals("true")) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 }
