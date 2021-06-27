8892aa1fb0849dd32c5a6eb0a960841be30793bd
tomdalton <email@tomdalton.de>
2021-06-27 15:18:03 +0200

    first

---
+++ character/src/de/TomDalton/Character/ListenerClass.java
@@ -0,0 +1,50 @@
+package de.TomDalton.Character;
+
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class ListenerClass implements org.bukkit.event.Listener{
+	
+	private Main plugin;
+
+	public ListenerClass(Main plugin) {
+		this.plugin = plugin;
+	}
+
+	/**
+	 * ungenutzt / nebenprojekt
+	@EventHandler
+	public void onAsyncPlayerChatEvent(AsyncPlayerChatEvent e) {
+		Player sender = e.getPlayer();
+		for (Player player : Bukkit.getOnlinePlayers()) {
+		    Location location = player.getLocation();
+		    if(e.getMessage().charAt(0)=='.') {
+		    	if (sender.getWorld().getName().equals(player.getWorld().getName()) && location.distanceSquared(sender.getLocation()) <= 5 * 5) {
+			        if(sender!=player) {
+			        	player.sendMessage(sender.getDisplayName() + " *flüstert* " + e.getMessage());
+			        }
+			        sender.sendMessage(sender.getDisplayName() + " *flüstert* " + e.getMessage());
+			        e.setCancelled(true);
+			    }
+		    }
+		}
+	}
+	*/
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		//plugin.loadPermissions(e.getPlayer());
+	}
+	
+	@EventHandler
+	public void onLeave(PlayerQuitEvent e) {
+		//plugin.perms.remove(e.getPlayer().getUniqueId());
+	}
+}
---
+++ character/src/de/TomDalton/Character/Main.java
@@ -0,0 +1,54 @@
+package de.TomDalton.Character;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import api.Api;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionAttachment;
+
+public class Main extends JavaPlugin{
+	
+	public FileConfiguration config;
+	public HashMap<UUID, PermissionAttachment> perms = new HashMap<UUID, PermissionAttachment>();
+	private File data;
+	private YamlConfiguration chardata;
+	
+	@Override
+	public void onEnable() {
+		createConfig();
+		initData();
+		createListener();
+		//new Group(this);
+		//new Permission(this);
+	}
+
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	public void createListener() {
+		Bukkit.getServer().getPluginManager().registerEvents(new ListenerClass(this), this);
+	}
+	
+	public void createConfig() {
+		this.saveDefaultConfig();
+		config = this.getConfig();
+		config.addDefault("nachrichten.prefix", "&f[&6Character&f]");
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
+	
+	public void initData() {
+		data = new File(this.getDataFolder(), "/data.yml");
+		chardata = YamlConfiguration.loadConfiguration(data);
+		Api.saveData(chardata,data);
+	}
+	
+}
